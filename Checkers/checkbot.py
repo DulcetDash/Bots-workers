f522e242b3b551c05bef862798e0acb195b7e6ef
Dominique Kanyik <domykanyiktesh01@gmail.com>
2022-05-04 04:45:54 +0200

    BW 1.0.4
    - Checkers bot added!
    - Critical bugs fix.

---
+++ Checkers/checkbot.py
@@ -0,0 +1,166 @@
+'''
+1. Get all the catalogue from Pick n Pay.
+'''
+# prettier-ignore
+from ast import Try
+from logging import root
+from math import ceil
+from tkinter import E
+from unicodedata import category
+from pymongo import MongoClient
+import time
+from random import randint
+import sys
+import os
+sys.path.append('../')
+from tqdm import tqdm
+import csv
+import re
+from bs4 import BeautifulSoup
+import requests
+from colorama import Back, Fore, Style, init
+init()
+import datetime
+import urllib.parse
+from scrapingant_client import ScrapingAntClient
+import http.client
+import json
+
+conn = http.client.HTTPSConnection("api.scrapingant.com")
+
+
+driver = False
+wait = False
+
+client = False
+db = False
+_SHOP_NAME_ = 'CHECKERS'
+
+def display_log(fore=Fore.GREEN, text=''):
+    print(fore + '{}'.format(text))
+    print(Style.RESET_ALL)
+
+display_log(Fore.GREEN, 'ROBOT NAME: CHECK-BOT')
+display_log(Fore.GREEN, _SHOP_NAME_)
+
+# Create a ScrapingAntClient instance
+headers = {
+   'x-api-key': "5ac50d549a1a43f2b4a9bda94d353b56",
+   'content-type': "application/json",
+   'accept': "application/json"
+}
+
+
+def getHTMLSOUPEDDocument(url):
+    payload = "{ \"url\": \""+ url +"\"}"
+    #1. Get the main page
+    conn.request("POST", "/v1/general", payload, headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    soup = BeautifulSoup(json.loads(data.decode("utf-8"))['content'], 'html.parser')
+    return soup
+
+
+def launchBot():
+    try:
+        client = MongoClient(
+                    'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false')
+
+        db = client.NEJ
+        collection_catalogue = db['catalogue_central']
+
+        root_url = 'https://www.checkers.co.za'
+
+        #1. Get the main categories
+        categories_page = getHTMLSOUPEDDocument(root_url)
+        #List all the categories
+        main_categories = categories_page.find_all('div', {'class':'department'})
+
+        display_log(Fore.BLUE, 'Finding store categories')
+        # print(main_categories)
+
+        for i, element in enumerate(main_categories):
+            if element.find('div', {'class':'department__caption'}) is not None:
+                category = str(element.find('h4', {'class':'department__title'}).get_text()).strip()
+                next_link = root_url + str(element.find('a')['href']).strip()
+
+                display_log(Fore.YELLOW, '[{}] {}'.format(i, category))
+                
+                category_spec_page = getHTMLSOUPEDDocument(next_link)
+
+                #Get the total number of pages
+                #https://www.checkers.co.za/c-2413/All-Departments/Food?q=%3Arelevance%3AbrowseAllStoresFacetOff%3AbrowseAllStoresFacetOff&page=1
+                pagination_data = int(str(category_spec_page.find('p', {'class':'total-number-of-results pull-right'}).get_text()).replace(' items','').replace(',','').strip())
+                page_number = ceil(pagination_data/20)
+
+                display_log(Fore.BLUE, '-> Page number found {}'.format(page_number))
+                print('Iterate over all the products')
+                
+                #Go through the pages
+                for k in range(page_number):
+                    #https://www.checkers.co.za/c-2413/All-Departments/Food?q=%3Arelevance%3AbrowseAllStoresFacetOff%3AbrowseAllStoresFacetOff&page=1
+                    addPage = '?q=%3Arelevance%3AbrowseAllStoresFacetOff%3AbrowseAllStoresFacetOff&page={}'.format(k) if k>0 else ''
+                    products_link = next_link + addPage
+
+                    #? Get the products infos
+                    products_megadata = getHTMLSOUPEDDocument(products_link).find_all('div', {'class':'item-product'})
+
+                    for j, product in enumerate(products_megadata):
+                        try:
+                            display_log(Fore.LIGHTRED_EX, '[*] {}% - {}'.format(round(i*100/len(main_categories)),category))
+                            display_log(Fore.CYAN, '{}% - {}'.format(round(k*100/page_number),'PAGE'))
+
+                            #Get the name, price and image of the product
+                            product_name = str(product.find('h3', {'class':'item-product__name'}).get_text()).strip()
+                            product_price = str(product.find('div', {'class':'special-price__price'}).get_text()).strip()
+                            product_image = root_url + str(product.find('div', {'class':'item-product__image __image'}).find('img')['src'])
+                            product_link = root_url + str(product.find('a')['href']).strip()
+
+                            #Save the model
+                            #? SAVE in the db
+                            #Compile the whole product data model
+                            TMP_DATA_MODEL = {
+                                'brand': _SHOP_NAME_,
+                                'product_name': product_name,
+                                'product_price': product_price,
+                                'product_picture': [product_image],
+                                'sku': str(product_name).upper().replace(' ','_'),
+                                'used_link': product_link,
+                                'meta': {
+                                    'category': str(category).upper().strip(),
+                                    'subcategory': str(category).upper().strip(),
+                                    'shop_name': _SHOP_NAME_,
+                                    'website_link': root_url,
+                                    'description': '',
+                                    },
+                                    'date_added':  datetime.datetime.today().replace(microsecond=0)
+                            }
+                            
+                            #! filter
+                            filterProduct = {
+                                'brand': _SHOP_NAME_,
+                                'product_name': product_name,
+                                'meta.category': str(category).upper().strip(),
+                                'meta.subcategory': str(category).upper().strip(),
+                                'meta.shop_name': _SHOP_NAME_
+                            }
+                            print(TMP_DATA_MODEL)
+                            display_log(Fore.GREEN, 'Saving the product model in catalogue')
+                            collection_catalogue.update_one(filterProduct, {"$set": TMP_DATA_MODEL}, upsert=True)
+                            print('----------')
+                        except Exception as e:
+                            print(e)
+                            continue
+
+            else:
+                display_log(Fore.LIGHTRED_EX, 'Invalid category')
+
+
+    except Exception as e:
+        print(e)
+
+
+#!DEBUG
+launchBot()
--- PEP/pepbot.py
+++ PEP/pepbot.py
@@ -43,7 +43,7 @@ display_log(Fore.GREEN, _SHOP_NAME_)
 
 # Create a ScrapingAntClient instance
 headers = {
-   'x-api-key': "53ea95eb0dd346f9a53ba3188a41a29e",
+   'x-api-key': "5ac50d549a1a43f2b4a9bda94d353b56",
    'content-type': "application/json",
    'accept': "application/json"
 }
@@ -81,126 +81,138 @@ def launchBot():
         for index,category_node in enumerate(main_categories):
             next_link = link + str(category_node.find('a', {'class':'btn btn-light menu-btn'})['href'])
             category = str(category_node.find('a', {'class':'btn btn-light menu-btn'}).get_text()).strip()
-            display_log(Fore.YELLOW, '[{}] {}'.format(index+1, category))
+            
+            if category.upper() not in ['MONEY','LAY-BY BUDDY', 'CATALOGUES']:
 
-            #Go through the subcategories
-            payload = "{ \"url\": \""+ next_link +"\"}"
-            #1. Get the main page
-            conn.request("POST", "/v1/general", payload, headers)
+                display_log(Fore.YELLOW, '[{}] {}'.format(index+1, category))
 
-            res = conn.getresponse()
-            data = res.read()
-
-            subcategory_data = BeautifulSoup(json.loads(data.decode("utf-8"))['content'], 'html.parser')
-            subcategories_all = subcategory_data.find_all('a', {'class':'image-content'})
-
-            #? Go inside the individual sbs
-            for i, sub_data in enumerate(subcategories_all):
-                sub_link = link +'/'+ str(sub_data['href']).strip()
-                subcategory_name = str(sub_data.get_text()).strip()
-
-                payload = "{ \"url\": \""+ sub_link +"\"}"
+                #Go through the subcategories
+                payload = "{ \"url\": \""+ next_link +"\"}"
                 #1. Get the main page
                 conn.request("POST", "/v1/general", payload, headers)
 
                 res = conn.getresponse()
                 data = res.read()
 
-                products_data_all = BeautifulSoup(json.loads(data.decode("utf-8"))['content'], 'html.parser')
+                subcategory_data = BeautifulSoup(json.loads(data.decode("utf-8"))['content'], 'html.parser')
+                subcategories_all = subcategory_data.find_all('a', {'class':'image-content'})
 
-                #? Get the total number of pages
-                pagination_data = products_data_all.find_all('li', {'class':'page-item'})
+                #? Go inside the individual sbs
+                for i, sub_data in enumerate(subcategories_all):
+                    sub_link = link +'/'+ str(sub_data['href']).strip()
+                    subcategory_name = str(sub_data.get_text()).strip()
 
-                pagination_data.pop()
-                page_number = int(pagination_data[len(pagination_data)-1].get_text())
-
-                display_log(Fore.BLUE, '-> Page number found {}'.format(page_number))
-                print('Iterate over all the products')
-                
-
-                #? Go through the pages
-                for page in range(page_number):
-                    #https://www.pepstores.com/products/ladies/outerwear?pageSize=20&currentPage=1
-                    addPage = "?pageSize=20&currentPage=" + str(page+1)
-                    products_link = sub_link + addPage
-
-                    payload = "{ \"url\": \""+ products_link +"\"}"
+                    payload = "{ \"url\": \""+ sub_link +"\"}"
                     #1. Get the main page
                     conn.request("POST", "/v1/general", payload, headers)
 
                     res = conn.getresponse()
                     data = res.read()
 
+                    products_data_all = BeautifulSoup(json.loads(data.decode("utf-8"))['content'], 'html.parser')
 
-                    products_megadata = BeautifulSoup(json.loads(data.decode("utf-8"))['content'], 'html.parser')
-                    products_mega_infos = products_megadata.find_all('a', {'class':'product-content d-flex flex-column'})
-                    #? Get all the products for this page
-                    for j, product_l1 in enumerate(products_mega_infos):
-                        display_log(Fore.CYAN, '{}% - {}'.format(round(i*100/len(subcategories_all)),subcategory_name))
+                    #? Get the total number of pages
+                    pagination_data = products_data_all.find_all('li', {'class':'page-item'})
 
-                        #Get the product's page link
-                        product_page_link = link + product_l1['href']
+                    pagination_data.pop()
+                    page_number = int(pagination_data[len(pagination_data)-1].get_text())
 
+                    display_log(Fore.BLUE, '-> Page number found {}'.format(page_number))
+                    print('Iterate over all the products')
+                    
 
-                        #? Do the final product lookup
-                        payload = "{ \"url\": \""+ product_page_link +"\"}"
+                    #? Go through the pages
+                    for page in range(page_number):
+                        #https://www.pepstores.com/products/ladies/outerwear?pageSize=20&currentPage=1
+                        addPage = "?pageSize=20&currentPage=" + str(page+1)
+                        products_link = sub_link + addPage
+
+                        payload = "{ \"url\": \""+ products_link +"\"}"
                         #1. Get the main page
                         conn.request("POST", "/v1/general", payload, headers)
 
                         res = conn.getresponse()
                         data = res.read()
 
-                        product_final_data = BeautifulSoup(json.loads(data.decode("utf-8"))['content'], 'html.parser')
 
-                        #Get the name, price, picture and sizes
-                        product_name = str(product_final_data.find('div',{'class':'product-wrapper'}).find('header').find('h1').get_text()).strip()
-                        product_price = str(product_final_data.find('div',{'class':'product-wrapper'}).find('span', {'class':'final-price'}).get_text()).strip()
-                        product_image = str(product_final_data.find('div', {'class':'gallery-wrap'}).find('img')['src']).strip()
-                        #Get the sizes
-                        product_sizes_data = product_final_data.find('div', {'class':'scrollinner'}).find_all('button')
+                        products_megadata = BeautifulSoup(json.loads(data.decode("utf-8"))['content'], 'html.parser')
+                        products_mega_infos = products_megadata.find_all('a', {'class':'product-content d-flex flex-column'})
+                        #? Get all the products for this page
+                        for j, product_l1 in enumerate(products_mega_infos):
+                            try:
+                                display_log(Fore.LIGHTRED_EX, '[*] {}% - {}'.format(round(index*100/len(main_categories)),category))
+                                display_log(Fore.CYAN, '{}% - {}'.format(round(i*100/len(subcategories_all)),subcategory_name))
 
-                        display_log(Fore.YELLOW,'{}% - {}'.format(round(j*100/len(products_mega_infos)),product_name))
+                                #Get the product's page link
+                                product_page_link = link + product_l1['href']
 
-                        product_sizes = []
 
-                        for size in product_sizes_data:
-                            product_sizes.append(str(size.get_text()).strip())
-                        
-                        #Save the model
-                        #? SAVE in the db
-                        #Compile the whole product data model
-                        TMP_DATA_MODEL = {
-                            'brand': _SHOP_NAME_,
-                            'product_name': product_name,
-                            'product_price': product_price,
-                            'product_picture': [product_image],
-                            'sku': str(product_name).upper().replace(' ','_'),
-                            'used_link': product_page_link,
-                            'meta': {
-                                'category': str(category).upper().strip(),
-                                'subcategory': str(subcategory_name).upper().strip(),
-                                'shop_name': _SHOP_NAME_,
-                                'website_link': 'https://app.debonairspizza.co.na/restaurant/8/debonairs-pizza-maerua-mall',
-                                'description': '',
-                                'options': {
-                                    'size': product_sizes
+                                #? Do the final product lookup
+                                payload = "{ \"url\": \""+ product_page_link +"\"}"
+                                #1. Get the main page
+                                conn.request("POST", "/v1/general", payload, headers)
+
+                                res = conn.getresponse()
+                                data = res.read()
+
+                                product_final_data = BeautifulSoup(json.loads(data.decode("utf-8"))['content'], 'html.parser')
+
+                                #Get the name, price, picture and sizes
+                                product_name = str(product_final_data.find('div',{'class':'product-wrapper'}).find('header').find('h1').get_text()).strip()
+                                product_price = str(product_final_data.find('div',{'class':'product-wrapper'}).find('span', {'class':'final-price'}).get_text()).strip()
+                                product_image = str(product_final_data.find('div', {'class':'gallery-wrap'}).find('img')['src']).strip()
+                                #Get the sizes
+                                print(payload)
+                                product_sizes_data = product_final_data.find('div', {'class':'scrollinner'}).find_all('button') if product_final_data.find('div', {'class':'scrollinner'}) is not None else []
+
+                                display_log(Fore.YELLOW,'{}% - {}'.format(round(j*100/len(products_mega_infos)),product_name))
+
+                                product_sizes = []
+
+                                for size in product_sizes_data:
+                                    product_sizes.append(str(size.get_text()).strip())
+                                
+                                #Save the model
+                                #? SAVE in the db
+                                #Compile the whole product data model
+                                TMP_DATA_MODEL = {
+                                    'brand': _SHOP_NAME_,
+                                    'product_name': product_name,
+                                    'product_price': product_price,
+                                    'product_picture': [product_image],
+                                    'sku': str(product_name).upper().replace(' ','_'),
+                                    'used_link': product_page_link,
+                                    'meta': {
+                                        'category': str(category).upper().strip(),
+                                        'subcategory': str(subcategory_name).upper().strip(),
+                                        'shop_name': _SHOP_NAME_,
+                                        'website_link': 'https://app.debonairspizza.co.na/restaurant/8/debonairs-pizza-maerua-mall',
+                                        'description': '',
+                                        'options': {
+                                            'size': product_sizes
+                                        }
+                                    },
+                                    'date_added':  datetime.datetime.today().replace(microsecond=0)
                                 }
-                            },
-                            'date_added':  datetime.datetime.today().replace(microsecond=0)
-                        }
 
-                        #! filter
-                        filterProduct = {
-                            'brand': _SHOP_NAME_,
-                            'product_name': product_name,
-                            'meta.category': str(category).upper().strip(),
-                            'meta.subcategory': str(subcategory_name).upper().strip(),
-                            'meta.shop_name': _SHOP_NAME_
-                        }
-                        print(TMP_DATA_MODEL)
-                        display_log(Fore.GREEN, 'Saving the product model in catalogue')
-                        collection_catalogue.update_one(filterProduct, {"$set": TMP_DATA_MODEL}, upsert=True)
-                        print('----------')
+                                #! filter
+                                filterProduct = {
+                                    'brand': _SHOP_NAME_,
+                                    'product_name': product_name,
+                                    'meta.category': str(category).upper().strip(),
+                                    'meta.subcategory': str(subcategory_name).upper().strip(),
+                                    'meta.shop_name': _SHOP_NAME_
+                                }
+                                print(TMP_DATA_MODEL)
+                                display_log(Fore.GREEN, 'Saving the product model in catalogue')
+                                collection_catalogue.update_one(filterProduct, {"$set": TMP_DATA_MODEL}, upsert=True)
+                                print('----------')
+                            except Exception as e:
+                                print(e)
+                                continue
+                
+            else:
+                print('Unauthorised category')
 
     
     except Exception as e:
